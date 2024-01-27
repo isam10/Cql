ca22cea2ed40e2405c777c2e05206e19927076c1
isam10 <157310973+isam10@users.noreply.github.com>
2024-01-28 01:07:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from langchain_helper import get_few_shot_db_chain
+
+st.title("Mr.Indicator 🚉")
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = get_few_shot_db_chain()
+    response = chain.run(question)
+
+    st.header("Answer")
+    st.write(response)
+
+
+
+
+
+
